8a8cef622fddc610cbf45f5eedd5736c5fec7287
sahithipaladi <86294777+sahithipaladi@users.noreply.github.com>
2021-07-10 08:06:00 +0530

    Update StockManagement.cs

--- StockAccountMangaement/StockManagement.cs
+++ StockAccountMangaement/StockManagement.cs
@@ -6,6 +6,7 @@ namespace StockAccountManagement
 {
     class StockManagement
     {
+        //listing the stock records
         public List<StockRecords> Stocks { get; set; }
 
         public class StockRecords
